11bef0be249388e848f449dbbdefefb65893287d
WyattZhang <zwuquan@gmail.com>
2014-11-16 18:56:19 +0800

    quickSort

--- 9.Sorting and Searching/QuickSort.playground/section-1.swift
+++ 9.Sorting and Searching/QuickSort.playground/section-1.swift
@@ -22,28 +22,3 @@ func quickSort(inout dataList: [Int], left: Int, right: Int) {
 var dataList = [42, 12, 88, 62, 63, 56, 1, 77, 88, 97, 97, 20, 45, 91, 62, 2, 15, 31, 59, 5]
 quickSort(&dataList, 0, dataList.count - 1)
 
-
-var randomNumbers = [42, 12, 88, 62, 63, 56, 1, 77, 88, 97, 97, 20, 45, 91, 62, 2, 15, 31, 59, 5]
-
-func partitionn(inout v: [Int], left: Int, right: Int) -> Int {
-    var i = left
-    for j in left...right {
-        if v[j] < v[left] {
-            i += 1
-            (v[i], v[j]) = (v[j], v[i])
-        }
-    }
-    (v[i], v[left]) = (v[left], v[i])
-    return i
-}
-
-func quicksort(inout v: [Int], left: Int, right: Int) {
-    if right > left {
-        let pivotIndex = partitionn(&v, left, right)
-        quicksort(&v, left, pivotIndex - 1)
-        quicksort(&v, pivotIndex + 1, right)
-    }
-}
-
-quicksort(&randomNumbers, 0, randomNumbers.count-1)
-
